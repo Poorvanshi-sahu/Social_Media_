a509149b4fb024e46d7af65a0dae8ce08736e7a7
Poorvanshi-sahu <poorvanshisahu@gmail.com>
2023-10-24 10:40:40 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express=require('express');
-const app=express();
-const cookieParser=require('cookie-parser');
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
 
-if(process.env.NODE_ENV!=="production"){
-    require("dotenv").config({path:"backend/config/config.env"});
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "/config/.env" });
 }
 
 // using middleware
-app.use(express.json({limit:"50mb"}))
-app.use(express.urlencoded({limit:"50mb",extended:true}))
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
-const post=require('./routes/post');
-const user=require('./routes/users');
+const post = require("./routes/post");
+const user = require("./routes/users");
 
-app.use("/api/v1",post);
-app.use("/api/v1",user);
+app.use("/api/v1", post);
+app.use("/api/v1", user);
 
-module.exports=app;    
+module.exports = app;
